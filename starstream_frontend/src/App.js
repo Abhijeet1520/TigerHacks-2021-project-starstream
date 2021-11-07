1eeb14b2177278e81d45eb3ff1c04f4db9de273c
Abhijeet Kumar <63132585+Abhijeet1520@users.noreply.github.com>
2021-11-07 21:44:35 +0530

    Add Galaxy.js, import react-router-dom

--- starstream_frontend/src/App.js
+++ starstream_frontend/src/App.js
@@ -1,37 +1,12 @@
-import { Suspense, useState, useEffect } from "react";
-import { Canvas } from "@react-three/fiber";
-// import { Environment, OrbitControls } from "@react-three/drei";
-import Star from "./star";
 import Home from "./Home";
+import {Router} from "react-router-dom";
 import "./styles.css";
 
 function App() {
 
-  const [state, setState] = useState([])
-  // useEffect(() => {
-  //   fetch("https://starstream-api-gateway-b2dzz7qh.uc.gateway.dev/min-data").then(
-  //     res => setState(res.data)
-  //   )
-  // }, [])
-
   return (
     <div className="app">
       <Home />
-
-      {/* <Canvas shadows dpr={[1, 2]} camera={{ position: [0, 0, 4], fov: 50 }}>
-        <ambientLight intensity={4} />
-        <spotLight
-          intensity={0.5}
-          angle={0.1}
-          penumbra={1}
-          position={[10, 15, 10]}
-          castShadow
-        />
-        <Suspense fallback={null}>
-          <Star />
-        </Suspense>
-        <OrbitControls autoRotate />
-      </Canvas> */}
     </div>
   );
 }
---
+++ starstream_frontend/src/Galaxy.js
@@ -0,0 +1,33 @@
+import { Suspense, useState, useEffect } from "react";
+import { Canvas } from "@react-three/fiber";
+import { Environment, OrbitControls } from "@react-three/drei";
+import Star from "./star";
+import "./styles.css";
+
+function Galaxy() {
+  const [state, setState] = useState([]);
+  useEffect(() => {
+    fetch(
+      "https://starstream-api-gateway-b2dzz7qh.uc.gateway.dev/min-data"
+    ).then((res) => setState(res.data));
+  }, []);
+
+  return (
+    <Canvas shadows dpr={[1, 2]} camera={{ position: [0, 0, 4], fov: 50 }}>
+      <ambientLight intensity={4} />
+      <spotLight
+        intensity={0.5}
+        angle={0.1}
+        penumbra={1}
+        position={[10, 15, 10]}
+        castShadow
+      />
+      <Suspense fallback={null}>
+        <Star />
+      </Suspense>
+      <OrbitControls autoRotate />
+    </Canvas>
+  );
+}
+
+export default Galaxy;
