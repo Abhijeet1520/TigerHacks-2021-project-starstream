7a41f2e53672fc16b26ced1bb2a04c2a68c8d431
Your Name <hr5359@srmist.edu.in>
2021-11-07 23:25:15 +0530

    ✨ Add Navbar

--- starstream_frontend/src/App.js
+++ starstream_frontend/src/App.js
@@ -1,5 +1,6 @@
 import Home from "./Home";
 import Galaxy from "./Galaxy";
+import Navbar from "./Navbar";
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 // import createHistory from "history/createBrowserHistory" 
 import "./styles.css";
@@ -8,6 +9,7 @@ function App() {
 
   return (
     <Router >
+      <Navbar />
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/galaxy" component={Galaxy} />
---
+++ starstream_frontend/src/Navbar.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+function Navbar({ handleLogout }) {
+    const [click, setClick] = useState(false);
+    const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false);
+
+    return (
+        <>
+            <nav className='navbar'>
+                <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
+                StarStream
+                </Link>
+                <div className='menu-icon' onClick={handleClick}>
+                    <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+                </div>
+                <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                    <li className='nav-item'>
+                        <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                            Home
+                        </Link>
+                    </li>
+                    <li className='nav-item'>
+                        <Link
+                            to='/galaxy'
+                            className='nav-links'
+                            onClick={closeMobileMenu}
+                        >
+                            Galaxy
+                        </Link>
+                    </li>
+                    <li className='nav-item'>
+                        <Link
+                            to='/stream'
+                            className='nav-links'
+                            onClick={closeMobileMenu}
+                        >
+                            Stream
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
+        </>
+    );
+}
+
+export default Navbar;
